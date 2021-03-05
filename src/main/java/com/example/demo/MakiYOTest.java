d81f72f510f35b36c5539f18e8e6e3dec7b46cca
ztczsunday <76503932+ztczsunday@users.noreply.github.com>
2021-03-05 17:55:38 +0800

    this is the second test

--- src/main/java/com/example/demo/MakiYOTest.java
+++ src/main/java/com/example/demo/MakiYOTest.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class MakiYOTest {
     public static void main(String[] args) {
         System.out.println("this is a test");
+        System.out.println("this is the second test");
     }
 }
