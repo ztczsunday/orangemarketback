36297192fe7482f2c2e11ea0ee2fb1addbdaf3eb
2183612059 <1025997470@qq.com>
2021-03-05 18:12:26 +0800

    删除GIT测试文件

--- src/main/java/com/example/demo/MakiYOTest.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-public class MakiYOTest {
-    public static void main(String[] args) {
-        System.out.println("this is a test");
-        System.out.println("this is the second test");
-    }
-}
