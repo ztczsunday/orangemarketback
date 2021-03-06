da37c9c3df94ecfd7877829e090a6f8ac6dabbdb
王鸿博 <2184312730>
2021-03-06 09:46:34 +0800

    initialize controller

--- src/main/java/com/example/demo/Core/DemoApplication.java
+++ src/main/java/com/example/demo/Core/DemoApplication.java
@@ -1,9 +1,9 @@
-package com.example.demo;
+package com.example.demo.Core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.example.demo"})
 public class DemoApplication {
 
     public static void main(String[] args) {
