509834ca64e13ee70b6aeaa011893f9df65af94c
王鸿博 <2184312730>
2021-03-08 17:57:18 +0800

    删除剩余的旧的多余类； 修正mybatis和springboot配置错误

--- src/main/java/com/orangeSoft/market/core/DemoApplication.java
+++ src/main/java/com/orangeSoft/market/core/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.orangeSoft.market.core;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages={"com.orangeSoft.market"})
+@MapperScan("com.orangeSoft.market.mapper")
 public class DemoApplication {
 
     public static void main(String[] args) {
