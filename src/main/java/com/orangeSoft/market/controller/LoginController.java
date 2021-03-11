baa11c12c463d92303445594a538ed62416d1284
MakiYO <ztczsunday@qq.com>
2021-03-11 18:17:25 +0800

    添加swagger,增加一个swagger例子

--- src/main/java/com/orangeSoft/market/controller/LoginController.java
+++ src/main/java/com/orangeSoft/market/controller/LoginController.java
@@ -2,8 +2,13 @@ package com.orangeSoft.market.controller;
 
 import com.google.gson.Gson;
 import com.orangeSoft.market.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @CrossOrigin
@@ -13,6 +18,11 @@ public class LoginController {
     @Autowired
     private Gson gson;
 
+    @ApiOperation(value = "登录方法",notes = "114514")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "account",value = "手机号",required = true,paramType = "form"),
+            @ApiImplicitParam(name = "password",value = "密码",required = true,paramType = "form")
+    })
     @PostMapping(value = "/login", produces = "application/json;charset=UTF-8")
     public String login(@RequestParam("account") String account,
                           @RequestParam("password") String password) {
--- src/main/java/com/orangeSoft/market/core/DemoApplication.java
+++ src/main/java/com/orangeSoft/market/core/DemoApplication.java
@@ -4,9 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = {"com.orangeSoft.market"})
 @MapperScan("com.orangeSoft.market.mapper")
+@EnableSwagger2
 public class DemoApplication extends WebMvcConfigurationSupport {
 
     public static void main(String[] args) {
