7179127f25d280ad191430749bf2ad59ef91f100
王鸿博 <2184312730>
2021-03-13 16:21:14 +0800

    与远端同步，调整分包结构; 上传sql更改;

---
+++ sql/2021.3.13修正.sql
@@ -0,0 +1,16 @@
+/** 添加新表 **/
+CREATE TABLE orangesoft.commodity_pictures (
+                                               pictures varchar(200) NULL,
+                                               pid BIGINT NULL,
+                                               cid BIGINT NULL,
+                                               CONSTRAINT commodity_pictures_pk PRIMARY KEY (pid),
+                                               CONSTRAINT commodity_pictures_FK FOREIGN KEY (cid) REFERENCES orangesoft.commodity(cid) ON DELETE CASCADE
+)
+    ENGINE=InnoDB
+    DEFAULT CHARSET=utf8mb4
+    COLLATE=utf8mb4_general_ci;
+ALTER TABLE orangesoft.commodity DROP COLUMN commodity_icon;
+ALTER TABLE orangesoft.commodity_pictures CHANGE pictures picture_url varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL;
+/** 增加主键 **/
+ALTER TABLE orangesoft.footprint ADD CONSTRAINT footprint_pk PRIMARY KEY (uid,cid);
+ALTER TABLE orangesoft.order_stateflow ADD CONSTRAINT order_stateflow_pk PRIMARY KEY (order_id,record_id);
