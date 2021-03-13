83811baa7a3d63fc79183b3f43af9ae28a3fc1ea
2183612059 <1025997470@qq.com>
2021-03-13 18:00:19 +0800

    彻底重构代码，使用MyBatis-Plus

--- src/main/java/com/orangeSoft/market/mapper/extend/ShopMapperE.java
+++
@@ -1,9 +0,0 @@
-package com.orangeSoft.market.mapper.extend;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
-
-@Mapper
-@Repository
-public interface ShopMapperE extends com.orangeSoft.market.mapper.ShopMapper {
-}
