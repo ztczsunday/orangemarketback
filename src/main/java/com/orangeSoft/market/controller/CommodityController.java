05a19f46bddd6c1fc775ce4e9978f636b4427038
王鸿博 <2184312730>
2021-03-15 11:35:30 +0800

    在CommodityController中增加跨域注解;

--- src/main/java/com/orangeSoft/market/controller/CommodityController.java
+++ src/main/java/com/orangeSoft/market/controller/CommodityController.java
@@ -4,11 +4,13 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.orangeSoft.market.common.utils.Result;
 import com.orangeSoft.market.service.impl.CommodityServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://localhost:8080")
 public class CommodityController {
     @Autowired
     CommodityServiceImpl commodityService;
