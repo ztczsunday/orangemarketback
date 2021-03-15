c738ae243a603f78cbb5bc3a2bab0e33b6993196
2183612059 <1025997470@qq.com>
2021-03-15 11:36:40 +0800

    TestController.java修改，增加了对Rest Controller的测试

--- src/main/java/com/orangeSoft/market/controller/TestController.java
+++ src/main/java/com/orangeSoft/market/controller/TestController.java
@@ -1,8 +1,13 @@
 package com.orangeSoft.market.controller;
 
+import com.orangeSoft.market.common.utils.Result;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class TestController {
@@ -19,5 +24,15 @@ public class TestController {
     @RequestMapping(value = "/loginFailure")
     public String toLoginError() {
         return "/failure";
+    }
+}
+@RestController
+class RestControllerTest {
+    @GetMapping(value = "/test", produces = "application/json;charset=UTF-8")
+    public Result.JSONResultMap test() {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1, 1);
+        map.put(2, 2);
+        return Result.success(map);
     }
 }
