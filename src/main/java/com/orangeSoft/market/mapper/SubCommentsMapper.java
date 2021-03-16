71dad481651f6f49c4e2de96b6dec9209da81a69
王鸿博 <2184312730>
2021-03-16 09:47:13 +0800

    修改数据库; 利用mybatis-plus生成文件;

---
+++ src/main/java/com/orangeSoft/market/mapper/SubCommentsMapper.java
@@ -0,0 +1,17 @@
+package com.orangeSoft.market.mapper;
+
+import com.orangeSoft.market.entity.SubComments;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @since 2021-03-16
+ */
+@Repository
+public interface SubCommentsMapper extends BaseMapper<SubComments> {
+
+}
---
+++ src/main/java/com/orangeSoft/market/service/ISubCommentsService.java
@@ -0,0 +1,15 @@
+package com.orangeSoft.market.service;
+
+import com.orangeSoft.market.entity.SubComments;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @since 2021-03-16
+ */
+public interface ISubCommentsService extends IService<SubComments> {
+
+}
