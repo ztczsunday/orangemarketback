c999632939208e48b84633dd8c3713f0030957ff
2183612059 <1025997470@qq.com>
2021-03-16 09:43:10 +0800

    精简代码实现

--- src/main/java/com/orangeSoft/market/common/utils/Result.java
+++ src/main/java/com/orangeSoft/market/common/utils/Result.java
@@ -1,9 +1,8 @@
 package com.orangeSoft.market.common.utils;
 
-import java.util.Collection;
+import com.sun.istack.internal.NotNull;
+
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * 结果类，用于返回给前端(Json格式)
@@ -12,7 +11,7 @@ public class Result {
     private static final String SUCCESS = "success";
     private static final String INFORMATION = "information";
     private static final String MESSAGE = "message";
-    private static ThreadLocal<JSONResultMap> threadLocal = new ThreadLocal<>();
+    private static final ThreadLocal<JSONResultMap> threadLocal = new ThreadLocal<>();
 
     private Result() {
     }
@@ -26,105 +25,46 @@ public class Result {
         return jsonResultMap;
     }
 
+    private static JSONResultMap setResultMap(boolean success, Object information, String message) {
+        return getJSONResultMap()
+                .putLink(SUCCESS, success)
+                .putLink(INFORMATION, information)
+                .putLink(MESSAGE, information);
+    }
+
+
     public static JSONResultMap success() {
-        return getJSONResultMap().put(SUCCESS, true).put(INFORMATION, "").put(MESSAGE, "");
+        return setResultMap(true, "", "");
     }
 
     public static JSONResultMap success(Object data) {
-        return getJSONResultMap().put(SUCCESS, true).put(INFORMATION, data).put(MESSAGE, "");
+        return setResultMap(true, data, "");
     }
 
     public static JSONResultMap success(Object data, String message) {
-        return getJSONResultMap().put(SUCCESS, true).put(INFORMATION, data).put(MESSAGE, message);
+        return setResultMap(true, data, message);
     }
 
     public static JSONResultMap fail(Object data) {
-        return getJSONResultMap().put(SUCCESS, false).put(INFORMATION, data).put(MESSAGE, "");
+        return setResultMap(false, data, "");
     }
 
     public static JSONResultMap fail() {
-        return getJSONResultMap().put(SUCCESS, false).put(INFORMATION, "").put(MESSAGE, "");
+        return setResultMap(false, "", "");
     }
 
     public static JSONResultMap fail(Object data, String message) {
-        return getJSONResultMap().put(SUCCESS, false).put(INFORMATION, data).put(MESSAGE, message);
+        return setResultMap(false, data, message);
     }
 
 
-    public static class JSONResultMap implements Map<String, Object> {
-        private Map<String, Object> localMap = new HashMap<>();
+    public static class JSONResultMap extends HashMap<String, Object> {
+        private JSONResultMap() {}
 
-        private JSONResultMap() {
-        }
-
-        private Map<String, Object> getLocalMap() {
-            return localMap;
-        }
-
-        @Override
-        public int size() {
-            return getLocalMap().size();
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return getLocalMap().isEmpty();
-        }
-
-        @Override
-        public boolean containsKey(Object key) {
-            return getLocalMap().containsKey(key);
-        }
-
-        @Override
-        public boolean containsValue(Object value) {
-            return getLocalMap().containsValue(value);
-        }
-
-        @Override
-        public Object get(Object key) {
-            return getLocalMap().get(key);
-        }
-
-        @Override
-        public JSONResultMap put(String key, Object value) {
-            getLocalMap().put(key, value);
+        @NotNull
+        public JSONResultMap putLink(String key, Object value) {
+            super.put(key, value);
             return this;
         }
-
-        @Override
-        public Object remove(Object key) {
-            return getLocalMap().remove(key);
-        }
-
-        @Override
-        public void putAll(Map<? extends String, ?> m) {
-            getLocalMap().putAll(m);
-        }
-
-
-        @Override
-        public void clear() {
-            getLocalMap().clear();
-        }
-
-        @Override
-        public Set<String> keySet() {
-            return getLocalMap().keySet();
-        }
-
-        @Override
-        public Collection<Object> values() {
-            return getLocalMap().values();
-        }
-
-        @Override
-        public Set<Entry<String, Object>> entrySet() {
-            return getLocalMap().entrySet();
-        }
-
-
     }
-
-
 }
