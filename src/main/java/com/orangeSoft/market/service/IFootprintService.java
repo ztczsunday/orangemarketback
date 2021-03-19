96c0d61f4328fcb481fb4d9b4ef720fbe9c9620b
MakiYO <ztczsunday@qq.com>
2021-03-19 11:18:40 +0800

    添加增加和删除历史记录的功能

--- src/main/java/com/orangeSoft/market/service/IFootprintService.java
+++ src/main/java/com/orangeSoft/market/service/IFootprintService.java
@@ -1,6 +1,10 @@
 package com.orangeSoft.market.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.orangeSoft.market.common.utils.Result;
+import com.orangeSoft.market.entity.Commodity;
 import com.orangeSoft.market.entity.Footprint;
 
 /**
@@ -12,4 +16,27 @@ import com.orangeSoft.market.entity.Footprint;
  */
 public interface IFootprintService extends IService<Footprint> {
 
+    /**
+     * 用户查看历史记录
+     *
+     * @param page 分页标签
+     * @return 该页历史记录
+     */
+    IPage<Commodity> findFootPrintByUid(Page<Footprint> page);
+
+    /**
+     * 删除历史记录
+     *
+     * @param cid 该历史记录对应的商品ID
+     * @return 是否删除完成
+     */
+    Result.JSONResultMap deleteFootPrint(long cid);
+
+    /**
+     * 添加历史记录
+     *
+     * @param cid 该历史记录对应的商品ID
+     * @return 是否添加完成
+     */
+    Result.JSONResultMap addFootPrint(long cid);
 }
