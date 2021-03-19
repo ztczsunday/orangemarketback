7385d0ed461f5b83f9bc6b1618cc8b96eaeb41a2
王鸿博 <2184312730>
2021-03-19 11:04:06 +0800

    解决GetAllLabels找不到的问题

--- src/main/java/com/orangeSoft/market/service/ICommodityLabelService.java
+++ src/main/java/com/orangeSoft/market/service/ICommodityLabelService.java
@@ -2,6 +2,7 @@ package com.orangeSoft.market.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.orangeSoft.market.entity.CommodityLabel;
+import com.orangeSoft.market.pojo.CommodityLabelResult;
 
 import java.util.List;
 
@@ -20,4 +21,19 @@ public interface ICommodityLabelService extends IService<CommodityLabel> {
      * @return 商品标签列表
      */
     List<CommodityLabel> getLabelsByCid(Long cid);
+
+    /**
+     * 查找当前所有商品标签
+     *
+     * @return 商品标签列表
+     */
+    List<CommodityLabelResult> getAllLabels();
+
+    /**
+     * 添加新标签
+     *
+     * @param labelName 新标签名
+     * @return 新增的标签对象
+     */
+    CommodityLabelResult addNewLabel(String labelName);
 }
--- src/main/java/com/orangeSoft/market/service/impl/CommodityLabelServiceImpl.java
+++ src/main/java/com/orangeSoft/market/service/impl/CommodityLabelServiceImpl.java
@@ -3,10 +3,13 @@ package com.orangeSoft.market.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.orangeSoft.market.entity.CommodityLabel;
 import com.orangeSoft.market.mapper.CommodityLabelMapper;
+import com.orangeSoft.market.pojo.CommodityLabelResult;
 import com.orangeSoft.market.service.ICommodityLabelService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -17,9 +20,30 @@ import java.util.List;
  */
 @Service
 public class CommodityLabelServiceImpl extends ServiceImpl<CommodityLabelMapper, CommodityLabel> implements ICommodityLabelService {
+    @Autowired
+    DataDictionaryServiceImpl dictionaryService;
 
     @Override
     public List<CommodityLabel> getLabelsByCid(Long cid) {
         return this.query().eq("cid", cid).list();
+    }
+
+    @Override
+    public List<CommodityLabelResult> getAllLabels() {
+        List<CommodityLabel> labelsId = this.query().list();
+        return labelsId.stream().map(label ->
+                new CommodityLabelResult(
+                        label,
+                        dictionaryService.query().eq("record_id", label.getRecordId()).one().getItemName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CommodityLabelResult addNewLabel(String labelName) {
+        if(dictionaryService.query().eq("item_name", labelName).count() > 0){
+            return null;
+        }
+
+        return null;
     }
 }
