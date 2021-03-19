415ddb49beae83f08775b2f569d8cf922b9b961d
MakiYO <ztczsunday@qq.com>
2021-03-19 18:10:52 +0800

    添加部分管理员功能

---
+++ src/main/java/com/orangeSoft/market/service/IAdminService.java
@@ -0,0 +1,41 @@
+package com.orangeSoft.market.service;
+
+import com.orangeSoft.market.common.utils.Result;
+
+/**
+ * <p>
+ * 服务类
+ * </p>
+ *
+ * @since 2021-03-19
+ */
+public interface IAdminService {
+    /**
+     * 查看用户总量
+     *
+     * @return 用户总数
+     */
+    Result.JSONResultMap findUserAmount();
+
+    /**
+     * 查看各个状态的订单数
+     *
+     * @return 各个状态的订单数
+     */
+    Result.JSONResultMap findOrderStatus();
+
+    /**
+     * 查看商品好评率
+     *
+     * @return 订单好评数与总数
+     */
+    Result.JSONResultMap findCommodityPraise();
+
+    /**
+     * 查看每天订单量
+     *
+     * @param days 查询天数
+     * @return 每天订单量
+     */
+    Result.JSONResultMap findEverydayOrderAmount(int days);
+}
