ebdd175480ca513b760cac871db1b7a802f109fd
王鸿博 <2184312730>
2021-03-20 18:23:06 +0800

    修正了获取到springsecurity用户名的问题;

--- src/main/java/com/orangeSoft/market/common/utils/MySessionUtil.java
+++ src/main/java/com/orangeSoft/market/common/utils/MySessionUtil.java
@@ -1,13 +1,20 @@
 package com.orangeSoft.market.common.utils;
 
 import com.orangeSoft.market.entity.UserInfo;
+import com.orangeSoft.market.service.impl.UserInfoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class MySessionUtil {
     private final static String CURRENT_USER = "currUser";
+    @Autowired
+    private static UserInfoServiceImpl userInfoService;
 
     public static UserInfo getCurrUser() {
-        return (UserInfo) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserInfo principal = (UserInfo) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserInfo currUser = userInfoService.getById(principal.getUid());
+        currUser.setUserPassword(principal.getPassword());
+        return currUser;
     }
 
 }
