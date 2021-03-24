19cadc0722f4f24d4b3a7859c4b1766d6cd560b1
2183612059 <1025997470@qq.com>
2021-03-24 14:52:19 +0800

    commit a cors configuration change: we now doesn't use cross origin

--- src/main/java/com/orangeSoft/market/controller/PayController.java
+++ src/main/java/com/orangeSoft/market/controller/PayController.java
@@ -20,20 +20,15 @@ public class PayController {
     @Autowired
     AlipayServiceImpl alipayService;
 
-    @ApiOperation(value = "支付宝", notes = "直接使用网址访问：http://localhost:8081/pay?orderId=订单号")
+    @ApiOperation(value = "支付宝", notes = "直接使用网址访问：http://localhost:8081/Alipay?orderId=订单号")
     @RequestMapping("/Alipay")
     @ResponseBody
     public void payController(long orderId, HttpServletResponse response) throws IOException, AlipayApiException {
         alipayService.Alipay(orderId, response);
     }
 
-    //    @ApiOperation(value = "支付宝异步通知")
-//    @PostMapping("/AlipayReturn")
-//    public String payReturnController(long trade_no, String trade_status){
-//        return alipayService.AlipayReturn(trade_no, trade_status);
-//    }
     @ApiOperation(value = "支付宝异步通知")
-    @RequestMapping(value = "/AlipayReturn", method = RequestMethod.POST)
+    @RequestMapping(value = "/AlipayReturn")
     public void alipayNotifyUrlInfo(HttpServletRequest request, HttpServletResponse response) {
         alipayService.notify(request, response);
     }
