e44001ec2147ff419e5699327562d9c0022bcdfb
MakiYO <ztczsunday@qq.com>
2021-03-24 10:20:28 +0800

    修改支付宝异步通知功能

--- src/main/java/com/orangeSoft/market/controller/PayController.java
+++ src/main/java/com/orangeSoft/market/controller/PayController.java
@@ -3,13 +3,19 @@ package com.orangeSoft.market.controller;
 import com.alipay.api.AlipayApiException;
 import com.orangeSoft.market.service.impl.AlipayServiceImpl;
 import io.swagger.annotations.ApiOperation;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.Map;
 
 @Controller
 public class PayController {
@@ -24,4 +30,16 @@ public class PayController {
     public void payController(long orderId, HttpServletResponse response) throws IOException, AlipayApiException {
         alipayService.Alipay(orderId, response);
     }
+
+    //    @ApiOperation(value = "支付宝异步通知")
+//    @PostMapping("/AlipayReturn")
+//    public String payReturnController(long trade_no, String trade_status){
+//        return alipayService.AlipayReturn(trade_no, trade_status);
+//    }
+    @ApiOperation(value = "支付宝异步通知")
+    @RequestMapping(value = "/AlipayReturn",method = RequestMethod.POST)
+    public void alipayNotifyUrlInfo(HttpServletRequest request,HttpServletResponse response){
+        alipayService.notify(request,response);
+    }
+
 }
